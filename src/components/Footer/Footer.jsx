a678a019e6fbab75fd8637cf576131018cbd04b9
maperezrf <95372481+maperezrf@users.noreply.github.com>
2024-10-25 18:06:13 -0500

    first commit

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -3,7 +3,6 @@ import React from 'react'
 export const Footer = () => {
     return (
         <footer>
-            <h1>footer</h1>
         </footer>
     );
 };
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,7 +2,6 @@
 export const Header = () => {
     return (
         <header>
-            <h1>header</h1>
         </header>
     );
 };
